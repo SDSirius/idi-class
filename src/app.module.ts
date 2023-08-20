b6df1f1a6fe8528729df621c6ccdbc5a4f5c9497
SDSirius <114844732+SDSirius@users.noreply.github.com>
2023-08-20 19:53:53 -0300

    começo do projeto/desafio

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt.guard';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    AuthModule,
+    UserModule,],
+  controllers: [],
+  providers: [
+    {provide: APP_GUARD, useClass: JwtAuthGuard}
+  ],
+})
+export class AppModule {}
---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,22 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+    @Prop({required: true})
+    name:string;
+
+    @Prop({required: true})
+    email:string;
+
+    @Prop({required: true})
+    password:string;
+
+    @Prop({required:false})
+    professor:boolean
+    
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
